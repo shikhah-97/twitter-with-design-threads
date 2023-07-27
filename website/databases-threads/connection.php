24132ee220153bd93ca4bfe4f08a53218405a565
Ghaidaalharby <140430131+Ghaidaalharby@users.noreply.github.com>
2023-07-27 20:59:55 +0300

    Add files via upload

---
+++ website/databases-threads/connection.php
@@ -0,0 +1,20 @@
+<?php 
+	$dsn = 'mysql:host=localhost; dbname=threads';
+	$user = 'root';
+	$password = '';
+ 
+
+	try{
+		$pdo = new PDO($dsn, $user, $password);
+	}catch(PDOException $e){
+		echo 'connection error! ' . $e;
+	}	
+?>
+
+
+
+
+
+
+
+
